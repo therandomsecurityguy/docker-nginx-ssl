310f227348faa620d8e34eaef0ae3f5eb82f9edf
TheRandomSecurityGuy <therandomsecurityguy@users.noreply.github.com>
2016-06-16 13:57:31 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ then
 fi
 
 
-DH="/etc/nginx/external/dh.pem"
+DH="/etc/nginx/external/dh2048.pem"
 
 if [ ! -e "$DH" ]
 then
@@ -22,7 +22,7 @@ fi
 if [ ! -e "/etc/nginx/external/cert.pem" ] || [ ! -e "/etc/nginx/external/key.pem" ]
 then
   echo ">> generating self signed cert"
-  openssl req -x509 -newkey rsa:4086 \
+  openssl req -x509 -newkey rsa:2048 \
   -subj "/C=XX/ST=XXXX/L=XXXX/O=XXXX/CN=localhost" \
   -keyout "/etc/nginx/external/key.pem" \
   -out "/etc/nginx/external/cert.pem" \
